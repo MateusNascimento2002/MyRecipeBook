8edc353320ca66b08846f40bf97e3a1152af75c3
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2024-06-04 11:02:45 -0300

    feat: database migrations code

---
+++ src/Backend/MyRecipeBook.Infrastructure/Extensions/ConfigureExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MyRecipeBook.Infrastructure.Extensions;
+
+public static class ConfigureExtension
+{
+    public static string ConnectionString(this IConfiguration configuration)
+    {
+        return configuration.GetConnectionString("ConnectionSQLServer")!;
+    }
+}
