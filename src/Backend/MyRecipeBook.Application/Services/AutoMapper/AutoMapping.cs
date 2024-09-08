3432468424cdda2672e8c1d558b1d024de5f8c8b
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2024-09-08 15:27:13 -0300

    feat: fluxo cadastro de receita

--- src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
+++ src/Backend/MyRecipeBook.Application/Services/AutoMapper/AutoMapping.cs
@@ -1,13 +1,17 @@
 ﻿using AutoMapper;
+using MyRecipeBook.Communication.Enums;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
+using Sqids;
 
 namespace MyRecipeBook.Application.Services.AutoMapper;
 
 public class AutoMapping : Profile
 {
-    public AutoMapping()
+    private readonly SqidsEncoder<long> _idEncoder;
+    public AutoMapping(SqidsEncoder<long> idEncoder)
     {
+        _idEncoder = idEncoder;
         RequestToDomain();
         DomainToResponse();
     }
@@ -17,11 +21,26 @@ public class AutoMapping : Profile
         CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
             .ForMember(u => u.Password, option => option.Ignore());
 
+        CreateMap<RequestRecipeJson, Domain.Entities.Recipe>()
+            .ForMember(r => r.Instructions, opt => opt.Ignore())
+            .ForMember(r => r.Ingredients, opt => opt.MapFrom(source => source.Ingredients.Distinct()))
+            .ForMember(r => r.DishTypes, opt => opt.MapFrom(source => source.DishTypes.Distinct()));
+
+        CreateMap<string, Domain.Entities.Ingredient>()
+            .ForMember(i => i.Item, opt => opt.MapFrom(source => source));
+
+        CreateMap<DishType, Domain.Entities.DishType>()
+            .ForMember(i => i.Type, opt => opt.MapFrom(source => source));
+
+        CreateMap<RequestInstructionJson, Domain.Entities.Instruction>();
+
     }
 
     private void DomainToResponse()
     {
         CreateMap<Domain.Entities.User, ResponseUserProfileJson>();
+        CreateMap<Domain.Entities.Recipe, ResponseRegisteredRecipeJson>()
+            .ForMember(dest => dest.Id, config => config.MapFrom(source => _idEncoder.Encode(source.Id)));
 
     }
 }
